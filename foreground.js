e834f2def49520085b781e4c9b8347dd3c2f0e9f
sharandeepak <sharandeepak32@gmail.com>
2020-10-23 17:17:41 +0530

    add foreground.js

---
+++ foreground.js
@@ -0,0 +1,10 @@
+console.log('from foreground')
+
+const first=document.createElement('button');
+first.innerText="SET DATA";
+first.id="first";
+
+const first=document.createElement('button');
+first.innerText="SET DATA";
+first.id="first";
+
